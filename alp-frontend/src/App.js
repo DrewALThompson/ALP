c57587f8a2a65402640a8e0329806302f34dab57
Drew Thompson <drewallynthompson@gmail.com>
2021-04-23 06:35:32 -0600

    More Dependencies and Router Transplant

--- alp-frontend/src/App.js
+++ alp-frontend/src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
-    <></>
+    <Router>
+      <NavBar />
+      
+      <Switch>
+        <Route exact path='/'>
+          <RouteContainer />
+        </Route>
+        <Route path='/about'>
+          <UserContainer />
+        </Route>
+        <Route path='/projects'>
+          <WordsContainer />
+        </Route>
+        <Route path='*'>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
